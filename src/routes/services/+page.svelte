6baeecc25c21bda9421e9a732a8f48b14b230efd
Frederick Akuffo <kofiakuffoboafo@gmail.com>
2025-02-26 16:22:13 +0000

    changed search toolbox

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,28 +1,41 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-    import Categories from '$modules/categories/index.svelte';
-	import { Button, Checkbox, Dropdown, Radio, Search } from 'flowbite-svelte';
+	import Categories from '$modules/categories/index.svelte';
+	import {
+		Button,
+		ButtonGroup,
+		Checkbox,
+		Dropdown,
+		Radio,
+		RadioButton,
+		Search
+	} from 'flowbite-svelte';
 	import List from './data/list.svelte';
 	const types = [
 		{ name: 'Ministry', checked: false },
 		{ name: 'Agency', checked: false },
 		{ name: 'Individual', checked: true },
-    { name: 'Business', checked: false },
+		{ name: 'Business', checked: false }
 	];
 
-    let currentFilter: 'Individual' | 'Business' | 'Ministry' | 'Agency' = 'Individual'
+	let currentFilter: 'Individual' | 'Business' | 'Ministry' | 'Agency' = 'Individual';
 	let searchTerm = '';
 </script>
 
-<div class="-mt-20 w-full flex flex-col justify-center items-center ">
-    <Button
-        class="bg-indigo-700 shadow-lg focus:ring-0 flex items-center justify-between py-3 w-1/5 z-40"
-	>Search services<Icon
-		icon="line-md:chevron-down"
-		class="ms-2 h-5 w-5 text-white dark:text-white"
-	/></Button
->
-<Dropdown class="h-44 overflow-y-auto text-black px-3 pb-3 text-sm">
+<div class="-mt-24 flex w-full flex-col items-center justify-center">
+	<div
+		class="z-40 flex gap-6 w-2/3 items-center justify-between rounded bg-white px-4 py-4 text-black shadow-lg focus:ring-0"
+	>
+	<ButtonGroup>
+		{#each types as t (t.name)}
+			<RadioButton value={t.name} bind:group={currentFilter}>{t.name}</RadioButton>
+		{/each}
+	</ButtonGroup>
+	<Search size="md" bind:value={searchTerm} />
+
+	</div>
+
+	<!-- <Dropdown class="h-44 overflow-y-auto text-black px-3 pb-3 text-sm">
 	<div slot="header" class="p-3">
 		<Search size="md" bind:value={searchTerm} />
 	</div>
@@ -37,9 +50,7 @@
 	>
     Check option to search by
     <Icon icon="gridicons:help-outline" class="ml-1 w-5 h-5" />	</div>
-</Dropdown>
+</Dropdown> -->
 </div>
 
-
 <List filter={currentFilter} {searchTerm} />
-
